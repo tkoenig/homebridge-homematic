f314c4e4c8f12ea6911023d12dff6de2dea71336
Thomas König <koenig.t@gmail.com>
2019-12-11 16:31:29 +0100

    ffff

--- ChannelServices/HmIPTemperatureAndHumidityControl.js
+++ ChannelServices/HmIPTemperatureAndHumidityControl.js
@@ -28,8 +28,10 @@ class HmIPTemperatureAndHumidityControl {
     // adress = 'HmIP-RF.000E58A991F047:1'
     let homematicId = address.match(/.+\.(.*):/)[1]
 
-    homematicEvents.on(homematicId + ':0', this.handleEvent)
-    homematicEvents.on(homematicId + ':1', this.handleEvent)
+    homematicEvents.on(homematicId + ':0', (data) => {
+      this.handleEvent(data)
+    })
+    homematicEvents.on(homematicId + ':1', (data) => { this.handleEvent(data) })
   }
 
   handleEvent (data) {
