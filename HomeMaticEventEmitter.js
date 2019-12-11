c078ec3a52fed1fa0345dd9af4292010af0a90d2
Thomas König <koenig.t@gmail.com>
2019-12-11 16:24:17 +0100

    ??

--- HomeMaticEventEmitter.js
+++ HomeMaticEventEmitter.js
@@ -4,6 +4,6 @@ class HomeMaticEventEmitter extends EventEmitter {}
 const homematicEvents = new HomeMaticEventEmitter()
 
 // prevents new properties from being added to the object
-Object.freeze(homematicEvents)
+// Object.freeze(homematicEvents)
 
 module.exports = homematicEvents
