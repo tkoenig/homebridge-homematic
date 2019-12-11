61ba973a0503169f85bccf3405b227d41b228bd1
Thomas König <koenig.t@gmail.com>
2019-12-11 16:23:22 +0100

    test eventemitter

--- ChannelServices/HmIPTemperatureAndHumidityControl.js
+++ ChannelServices/HmIPTemperatureAndHumidityControl.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const homematicEvents = require('../HomeMaticEventEmitter.js')
 class HmIPTemperatureAndHumidityControl {
   constructor (
     log,
@@ -16,14 +17,23 @@ class HmIPTemperatureAndHumidityControl {
   ) {
     Object.assign(this, { log, platform, id, name, type, address, special, cfg, service, characteristic, deviceType })
     // log.warn('platform: %(s)', platform)
-    log.warn('id: %(s)', id)
-    log.warn('name: %(s)', name)
-    log.warn('type: %(s)', type)
-    log.warn('address: %(s)', address)
-    log.warn('special: %(s)', special)
-    log.warn('cfg: %(s)', cfg)
-    log.warn('characteristic: %(s)', characteristic)
-    log.warn('deviceType: %(s)', deviceType)
+    // log.warn('id: %(s)', id)
+    // log.warn('name: %(s)', name)
+    // log.warn('type: %(s)', type)
+    // log.warn('address: %(s)', address)
+    // log.warn('special: %(s)', special)
+    // log.warn('cfg: %(s)', cfg)
+    // log.warn('characteristic: %(s)', characteristic)
+    // log.warn('deviceType: %(s)', deviceType)
+    // adress = 'HmIP-RF.000E58A991F047:1'
+    let homematicId = address.match(/.+\.(.*):/)[1]
+
+    homematicEvents.on(homematicId + ':0', this.handleEvent)
+    homematicEvents.on(homematicId + ':1', this.handleEvent)
+  }
+
+  handleEvent (data) {
+    this.log.warn('received data:', data)
   }
 
   // Used in HomeMaticChannelLoader:91..
