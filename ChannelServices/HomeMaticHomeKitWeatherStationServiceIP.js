16d6bf46a044d59543867d0afbd1ec45b8a98907
Thomas König <tkoenig@users.noreply.github.com>
2024-11-21 21:28:04 +0100

    try fixing slow request...

--- ChannelServices/HomeMaticHomeKitWeatherStationServiceIP.js
+++ ChannelServices/HomeMaticHomeKitWeatherStationServiceIP.js
@@ -130,7 +130,11 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
     .setProps({ minValue: -100 })
     .on('get', function (callback) {
       that.query('ACTUAL_TEMPERATURE', function (value) {
-        if (callback) callback(null, value)
+        if (value !== undefined) {
+          callback(null, value);
+        } else {
+          callback();
+        }
       })
     })
 
@@ -143,7 +147,11 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
   this.chum = humidity.getCharacteristic(Characteristic.CurrentRelativeHumidity)
     .on('get', function (callback) {
       that.query('HUMIDITY', function (value) {
-        if (callback) callback(null, value)
+        if (value !== undefined) {
+          callback(null, value);
+        } else {
+          callback();
+        }
       })
     })
 
@@ -156,7 +164,11 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
   this.cbright = brightness.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
     .on('get', function (callback) {
       that.query('ILLUMINATION', function (value) {
-        if (callback) callback(null, value)
+        if (value !== undefined) {
+          callback(null, value);
+        } else {
+          callback();
+        }
       })
     })
 
@@ -169,8 +181,12 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
   this.csunshineduration = sunshineduration.getCharacteristic(Characteristic.SunshineCharacteristic)
     .on('get', function (callback) {
       this.query('SUNSHINEDURATION', function (value) {
-        if (callback) callback(null, value)
-      })
+        if (value !== undefined) {
+          callback(null, value);
+        } else {
+          callback();
+        }
+      });
     }.bind(this))
 
   this.setCurrentStateCharacteristic('SUNSHINEDURATION', this.csunshineduration)
@@ -183,8 +199,12 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
     .setProps({ minValue: 0.0, maxValue: 150.0, minStep: 1.0 })
     .on('get', function (callback) {
       this.query('WIND_SPEED', function (value) {
-        if (callback) callback(null, value)
-      })
+        if (value !== undefined) {
+          callback(null, value);
+        } else {
+          callback();
+        }
+      });
     }.bind(this))
 
   this.setCurrentStateCharacteristic('WIND_SPEED', this.cwindspeed)
@@ -211,7 +231,11 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
     this.craincount = raincount.getCharacteristic(Characteristic.RainCountCharacteristic)
       .on('get', function (callback) {
         this.query('RAIN_COUNTER', function (value) {
-          if (callback) callback(null, value)
+          if (value !== undefined) {
+            callback(null, value);
+          } else {
+            callback();
+          }
         })
       }.bind(this))
 
@@ -227,7 +251,11 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
     this.cwinddirection = winddirection.getCharacteristic(Characteristic.WindDirectionCharacteristic)
       .on('get', function (callback) {
         this.query('WIND_DIR', function (value) {
-          if (callback) callback(null, value)
+          if (value !== undefined) {
+            callback(null, value);
+          } else {
+            callback();
+          }
         })
       }.bind(this))
 
@@ -240,7 +268,11 @@ HomeMaticHomeKitWeatherStationServiceIP.prototype.createDeviceService = function
     this.cwindrange = windrange.getCharacteristic(Characteristic.WindRangeCharacteristic)
       .on('get', function (callback) {
         this.query('WIND_DIR_RANGE', function (value) {
-          if (callback) callback(null, value)
+          if (value !== undefined) {
+            callback(null, value);
+          } else {
+            callback();
+          }
         })
       }.bind(this))
 
